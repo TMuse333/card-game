f304a6e1bdaaae7522583094be6409341e5395d7
Thomas Musial <th896493@dal.ca>
2023-08-09 19:21:57 -0300

    styled the result screen

--- frontEnd/src/components/ResultScreen.jsx
+++ frontEnd/src/components/ResultScreen.jsx
@@ -38,16 +38,14 @@ const ResultScreen = ({ win, score, startClicked}) => {
 
   const styles = {
     display: win === null ? 'none' : 'block',
-    height: '70vw',
-    width: '50vw',
+    
     marginLeft: 'auto',
     marginRight: 'auto',
     marginTop: '3rem',
     zIndex: -5,
     animation: win && score <=100 && !startClicked? 'moveAndScaleClown 1.5s infinite ease' : 'scaleUp 1.5s ease',
     transition: ' transform 1s ease',
-    maxHeight: '600px',
-    maxWidth: '400px',
+  
   //  position: 'relative'
 
   };
@@ -72,25 +70,6 @@ const ResultScreen = ({ win, score, startClicked}) => {
     
   };
 
-  const dataStyle = {
-    fontFamily: 'Roboto, sans-serif',
-    display: !showStats ? 'none' : 'block',
-    fontSize: '1rem',
-    color: 'black',
-    background: 'grey',
-   // background: 'linear-gradient(45deg, #ff00ff, #33cc33, #0000ff)',
-    backgroundSize: '200% 200%',
-   /// borderRadius: '10px',
-    width: '80%',
-    height: '2rem',
-    maxHeight: '300px',
-    maxWidth: '200px',
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    transform: 'translateY(10rem)',
-    //animation: 'animateGradient 10s linear infinite',
-    paddingTop: '5px'
-  };
   
   
   
@@ -106,7 +85,8 @@ const ResultScreen = ({ win, score, startClicked}) => {
     <>
 
   
-      <img src={ win && score > 100 ? saiyans : win && score < 101? clown : null} style={styles} alt="Result" />
+      <img src={ win && score > 100 ? saiyans : win && score < 101? clown : null} style={styles} alt="Result"
+      className='result-img' />
     
       <div style={textStyle}>
         <p>{win && score > 100 ? ` Your score was ${score}` : score < 101 && win ? `Get your points up playa! You only scored ${score}` : null}</p>
